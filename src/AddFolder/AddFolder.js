792a7165737b81f2fdbc67876ad107d80b7d7845
Ben Cohen <benjcohen1985@gmail.com>
2019-06-21 15:52:10 -0400

    Add state for backend functionality

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,26 +1,49 @@
-import React, { Component } from 'react'
-import NotefulForm from '../NotefulForm/NotefulForm'
-import './AddFolder.css'
+import React, { Component } from "react";
+import NotefulForm from "../NotefulForm/NotefulForm";
+import "./AddFolder.css";
 
 export default class AddFolder extends Component {
+  state = {
+    name: ""
+  };
+  handleFolderName = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    fetch("http://localhost:8000/api/folders", {
+      method: "POST",
+      body: JSON.stringify({
+        name: this.state.name
+      }),
+      headers: {
+        "content-type": "application/json"
+      }
+    }).then(res => console.log(res));
+  };
   render() {
+    console.log(this.state.name);
+
     return (
-      <section className='AddFolder'>
+      <section className="AddFolder">
         <h2>Create a folder</h2>
-        <NotefulForm>
-          <div className='field'>
-            <label htmlFor='folder-name-input'>
-              Name
-            </label>
-            <input type='text' id='folder-name-input' />
+        <NotefulForm onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label htmlFor="folder-name-input">Name</label>
+            <input
+              type="text"
+              value={this.state.name}
+              id="folder-name-input"
+              onChange={this.handleFolderName}
+            />
           </div>
-          <div className='buttons'>
-            <button type='submit'>
-              Add folder
-            </button>
+          <div className="buttons">
+            <button type="submit">Add folder</button>
           </div>
         </NotefulForm>
       </section>
-    )
+    );
   }
 }
--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,49 +1,98 @@
-import React, { Component } from 'react'
-import NotefulForm from '../NotefulForm/NotefulForm'
-import './AddNote.css'
+import React, { Component } from "react";
+import NotefulForm from "../NotefulForm/NotefulForm";
+import "./AddNote.css";
+import { Redirect } from "react-router";
 
 export default class AddNote extends Component {
   static defaultProps = {
-    folders: [],
+    folders: []
+  };
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      content: "",
+      folder: ""
+    };
   }
+  handleNameChange = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+  handleContentInput = e => {
+    this.setState({
+      content: e.target.value
+    });
+  };
+  handleClick = e => {
+    this.setState({
+      redirect: true
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    fetch("http://localhost:8000/api/notes", {
+      method: "POST",
+      body: JSON.stringify({
+        note_name: this.state.name,
+        content: this.state.content
+      }),
+      headers: {
+        "content-type": "application/json"
+      }
+    }).then(res => console.log(res));
+  };
   render() {
-    const { folders } = this.props
+    if (this.state.redirect) {
+      return <Redirect push to="/" />;
+    }
+    const { folders } = this.props;
+
     return (
-      <section className='AddNote'>
+      <section className="AddNote">
         <h2>Create a note</h2>
-        <NotefulForm>
-          <div className='field'>
-            <label htmlFor='note-name-input'>
-              Name
-            </label>
-            <input type='text' id='note-name-input' />
+        <NotefulForm onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label htmlFor="note-name-input">Name</label>
+            <input
+              type="text"
+              id="note-name-input"
+              value={this.state.name}
+              onChange={this.handleNameChange}
+            />
           </div>
-          <div className='field'>
-            <label htmlFor='note-content-input'>
-              Content
-            </label>
-            <textarea id='note-content-input' />
+          <div className="field">
+            <label htmlFor="note-content-input">Content</label>
+            <textarea
+              id="note-content-input"
+              type="text"
+              value={this.state.content}
+              onChange={this.handleContentInput}
+            />
           </div>
-          <div className='field'>
-            <label htmlFor='note-folder-select'>
-              Folder
-            </label>
-            <select id='note-folder-select'>
+          <div className="field">
+            <label htmlFor="note-folder-select">Folder</label>
+            <select
+              id="note-folder-select"
+              // onChange={this.setState({})}
+            >
               <option value={null}>...</option>
-              {folders.map(folder =>
+              {folders.map(folder => (
                 <option key={folder.id} value={folder.id}>
                   {folder.name}
                 </option>
-              )}
+              ))}
             </select>
           </div>
-          <div className='buttons'>
-            <button type='submit'>
+          <div className="buttons">
+            <button type="submit" onClick={this.handleClick}>
               Add note
             </button>
           </div>
         </NotefulForm>
       </section>
-    )
+    );
   }
 }
