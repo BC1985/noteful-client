354f0c9976fc07d9fa2cf9820d7d1736e2464761
Ben Cohen <benjcohen1985@gmail.com>
2019-06-21 21:04:10 -0400

    Add delete function

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,31 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { format } from 'date-fns'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './Note.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { format } from "date-fns";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Note(props) {
-  return (
-    <div className='Note'>
-      <h2 className='Note__title'>
-        <Link to={`/note/${props.id}`}>
-          {props.name}
-        </Link>
-      </h2>
-      <button className='Note__delete' type='button'>
-        <FontAwesomeIcon icon='trash-alt' />
-        {' '}
-        remove
-      </button>
-      <div className='Note__dates'>
-        <div className='Note__dates-modified'>
-          Modified
-          {' '}
-          <span className='Date'>
-            {format(props.modified, 'Do MMM YYYY')}
-          </span>
+import "./Note.css";
+
+export default class Note extends React.Component {
+  handleDelete = e => {
+    let id = this.props.id;
+    console.log(id);
+
+    e.preventDefault();
+    fetch(`http://localhost:8000/api/notes/${id}`, {
+      method: "DELETE"
+    })
+      .then(res => console.log(res))
+      .catch(error => console.error(error));
+  };
+  render() {
+    return (
+      <div className="Note">
+        <h2 className="Note__title">
+          <Link to={`/notes/${this.props.id}`}>{this.props.name}</Link>
+        </h2>
+        <button
+          className="Note__delete"
+          type="button"
+          onClick={this.handleDelete}
+        >
+          <FontAwesomeIcon icon="trash-alt" /> remove
+        </button>
+        <div className="Note__dates">
+          <div className="Note__dates-modified">
+            Modified{" "}
+            <span className="Date">
+              {format(this.props.modified, "Do MMM YYYY")}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
