29a02ffe9826135c6d8168d87fc672c6e8e5d687
Ben Cohen <benjcohen1985@gmail.com>
2019-06-22 12:44:03 -0400

    Remove modified date reference from notes

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { format } from "date-fns";
+// import { format } from "date-fns";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import "./Note.css";
@@ -31,14 +31,14 @@ export default class Note extends React.Component {
           <FontAwesomeIcon icon="trash-alt" /> remove
         </button>
         <div className="Note_content">{this.props.content}</div>
-        <div className="Note__dates">
+        {/* <div className="Note__dates">
           <div className="Note__dates-modified">
             Modified{" "}
             <span className="Date">
               {format(this.props.modified, "Do MMM YYYY")}
             </span>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
